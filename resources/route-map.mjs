b63cf7145c8ada65d0949eef3862ff6b9fdfc94e
TheDongle <stevewalkerbeta@gmail.com>
2024-06-03 09:22:33 +0100

    Added RouteMap class with clear distinction between secure/insecure routes

---
+++ resources/route-map.mjs
@@ -0,0 +1,46 @@
+import { ifLoggedIn } from "./session/borderControl.mjs";
+class RouteMap {
+  #routes;
+  constructor() {
+    this.#routes = new Map();
+  }
+  static ifLoggedIn = ifLoggedIn;
+  /**
+   * @param {object} routes
+   */
+  set secureRoutes(routes) {
+    for (const [key, arr] of Object.entries(routes)) {
+      this.#routes.set(key, [RouteMap.ifLoggedIn, ...arr]);
+    }
+  }
+  /**
+   * @param {object} routes
+   */
+  set insecureRoutes(routes) {
+    for (const [key, arr] of Object.entries(routes)) {
+      this.#routes.set(key, arr);
+    }
+  }
+  /**
+   * @param {string} routes
+   */
+  get(key) {
+    return this.#routes.get(key);
+  }
+  /**
+   * @param {array} keys
+   * @param {array} fns
+   */
+  addCommonPrefix(keys, fns) {
+    keys.forEach((key) => this.#routes.set(key, fns.concat(this.#routes.get(key))));
+  }
+  /**
+   * @param {array} keys
+   * @param {array} fns
+   */
+  addCommonSuffix(keys, fns) {
+    keys.forEach((key) => this.#routes.set(key, this.#routes.get(key).concat(fns)));
+  }
+}
+
+export { RouteMap };
--- routes/games.mjs
+++ routes/games.mjs
@@ -1,10 +1,27 @@
 import express from "express";
 const router = express.Router();
-import { RouteMap } from "./games_middlewear/route_map.mjs";
+import { RouteMap } from "../resources/route-map.mjs";
+import { setPageView, showGames } from "./games_middlewear/index.mjs";
+import { setTableView } from "./games_middlewear/reset.mjs";
+import { searchforGame } from "./games_middlewear/_new.mjs";
+import { createGame } from "./games_middlewear/create.mjs";
+import { destroyGame } from "./games_middlewear/destroy.mjs";
+import { ifReqNotEmpty } from "../resources/session/borderControl.mjs";
 
-router.get("/", RouteMap.get("ShowPage"));
-router.get("/new", RouteMap.get("Search"));
-router.post("/", RouteMap.get("Create"));
-router.delete("/:title", RouteMap.get("Destroy"));
+const routeMap = new RouteMap();
+routeMap.secureRoutes = {
+  ShowPage: [setPageView, showGames],
+  Search: [searchforGame],
+  Create: [createGame],
+  Destroy: [destroyGame],
+};
+routeMap.addCommonPrefix(["Create", "Destroy"], [ifReqNotEmpty]);
+routeMap.addCommonSuffix(["Create", "Destroy"], [setTableView, showGames]);
 
-export { router, RouteMap };
+router.get("/", routeMap.get("ShowPage"));
+router.get("/new", routeMap.get("Search"));
+router.post("/", routeMap.get("Create"));
+router.delete("/:title", routeMap.get("Destroy"));
+
+export { router };
+
