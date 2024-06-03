80c902e9acf5b441db48cc056b3ceaa4ccd955ee
TheDongle <stevewalkerbeta@gmail.com>
2024-06-03 09:48:22 +0100

    pre-Initiated Routemap for Users added to middlewear module

--- routes/users.mjs
+++ routes/users.mjs
@@ -1,37 +1,15 @@
-import { ifReqNotEmpty, ifLoggedIn } from "../resources/session/borderControl.mjs";
 import express from "express";
 const router = express.Router();
+import { serve } from "./users_middlewear/mapping.mjs";
 
-import { skipLogin, loginPage } from "./users_middlewear/index.mjs";
-import { userLogin } from "./users_middlewear/login.mjs";
-import { destroySession } from "./users_middlewear/logout.mjs";
-import { getForm } from "./users_middlewear/_new.mjs";
-import { createUser, createGuest } from "./users_middlewear/create.mjs";
-import { update } from "./users_middlewear/update.mjs";
-import { getDetails } from "./users_middlewear/show.mjs";
-import { RouteMap } from "../resources/route-map.mjs";
 
-const routeMap = new RouteMap();
-routeMap.secureRoutes = {
-  Logout: [destroySession],
-  UpdateDetails: [update],
-  ShowDetails: [getDetails],
-};
-routeMap.insecureRoutes = {
-  Index: [skipLogin, loginPage],
-  Login: [userLogin],
-  ShowRegisterForm: [getForm],
-  Register: [createGuest, createUser],
-};
-routeMap.addCommonPrefix(["Login", "Register"], [ifReqNotEmpty]);
-
-router.get("/", routeMap.get("Index"));
-router.post("/", routeMap.get("Login"));
-router.delete("/", routeMap.get("Logout"));
-router.get("/new", routeMap.get("ShowRegisterForm"));
-router.post("/new", routeMap.get("Register"));
-router.patch("/details", routeMap.get("UpdateDetails"));
-router.get("/details", routeMap.get("ShowDetails"));
+router.get("/", serve("Index"));
+router.post("/", serve("Login"));
+router.delete("/", serve("Logout"));
+router.get("/new", serve("ShowRegisterForm"));
+router.post("/new", serve("Register"));
+router.patch("/details", serve("UpdateDetails"));
+router.get("/details", serve("ShowDetails"));
 
 export { router };
 
---
+++ routes/users_middlewear/mapping.mjs
@@ -0,0 +1,27 @@
+import { destroySession } from "./logout.mjs";
+import { update } from "./update.mjs";
+import { getDetails } from "./show.mjs";
+import { skipLogin, loginPage } from "./index.mjs";
+import { userLogin } from "./login.mjs";
+import { getForm } from "./_new.mjs";
+import { createGuest, createUser } from "./create.mjs";
+import { ifReqNotEmpty } from "../../resources/session/borderControl.mjs";
+import { RouteMap } from "../../resources/route-map.mjs";
+
+const usersMap = new RouteMap();
+
+usersMap.secureRoutes = {
+  Logout: [destroySession],
+  UpdateDetails: [update],
+  ShowDetails: [getDetails],
+};
+usersMap.insecureRoutes = {
+  Index: [skipLogin, loginPage],
+  Login: [userLogin],
+  ShowRegisterForm: [getForm],
+  Register: [createGuest, createUser],
+};
+usersMap.addCommonPrefix(["Login", "Register"], [ifReqNotEmpty]);
+
+
+export const serve = (key) => usersMap.get(key);
