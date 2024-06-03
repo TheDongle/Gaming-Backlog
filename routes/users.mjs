638c6ff19e854825a4cbfc2141726e69fa5e0f37
TheDongle <stevewalkerbeta@gmail.com>
2024-06-03 09:32:59 +0100

    Moved RouteMap logic to User Router

--- routes/users.mjs
+++ routes/users.mjs
@@ -9,22 +9,29 @@ import { getForm } from "./users_middlewear/_new.mjs";
 import { createUser, createGuest } from "./users_middlewear/create.mjs";
 import { update } from "./users_middlewear/update.mjs";
 import { getDetails } from "./users_middlewear/show.mjs";
+import { RouteMap } from "../resources/route-map.mjs";
 
-const Index = [skipLogin, loginPage];
-const Login = [ifReqNotEmpty, userLogin];
-const Logout = [ifLoggedIn, destroySession];
-const ShowRegisterForm = [getForm];
-const Register = [ifReqNotEmpty, createGuest, createUser];
-const UpdateDetails = [ifLoggedIn, update];
-const ShowDetails = [ifLoggedIn, getDetails];
+const routeMap = new RouteMap();
+routeMap.secureRoutes = {
+  Logout: [destroySession],
+  UpdateDetails: [update],
+  ShowDetails: [getDetails],
+};
+routeMap.insecureRoutes = {
+  Index: [skipLogin, loginPage],
+  Login: [userLogin],
+  ShowRegisterForm: [getForm],
+  Register: [createGuest, createUser],
+};
+routeMap.addCommonPrefix(["Login", "Register"], [ifReqNotEmpty]);
 
-router.get("/", Index);
-router.post("/", Login);
-router.delete("/", Logout);
-router.get("/new", ShowRegisterForm);
-router.post("/new", Register);
-router.patch("/details", UpdateDetails);
-router.get("/details", ShowDetails);
+router.get("/", routeMap.get("Index"));
+router.post("/", routeMap.get("Login"));
+router.delete("/", routeMap.get("Logout"));
+router.get("/new", routeMap.get("ShowRegisterForm"));
+router.post("/new", routeMap.get("Register"));
+router.patch("/details", routeMap.get("UpdateDetails"));
+router.get("/details", routeMap.get("ShowDetails"));
 
 export { router };
 
