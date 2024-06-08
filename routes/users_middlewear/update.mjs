14084924dfc9ef0a04cc789ca3c85ad6c1186b19
TheDongle <stevewalkerbeta@gmail.com>
2024-06-08 19:12:46 +0100

    Added functionality for Update User - local variables for views are also updated ASAP

--- routes/users_middlewear/update.mjs
+++ routes/users_middlewear/update.mjs
@@ -4,8 +4,18 @@ import bcrypt, { hashSync } from "bcrypt";
 
 async function update(req, res, next) {
   try {
+    // Update db
     const db = req.app.get("db");
-    await db.update(req.session.user, req.body);
+    const user = await db.update(req.session.user, req.body);
+    if (user === null) {
+      throw createError(500, "update unsuccessful");
+    }
+    // Update Views
+    for (let [key, val] of Object.entries(req.body)) {
+      if (key === "password") continue;
+      req.app.locals[key] = val;
+    }
+    // Success
     res.send("Update Successful");
   } catch (err) {
     next(err);
--- routes/users_middlewear/update.test.mjs
+++ routes/users_middlewear/update.test.mjs
@@ -2,31 +2,43 @@ import { expect, jest, test } from "@jest/globals";
 import request from "supertest";
 import { default as MakeApp } from "../../app.mjs";
 
+const session = jest.fn((options) => (req, res, next) => {
+  req.session = { user: "1" };
+  next();
+});
+const startingUser = { username: "jerry", password: "kidneysStone1", _id: "1" };
+const update = jest.fn((_, params) => Object.assign(startingUser, params));
+const find = jest.fn(() => startingUser);
+const app = MakeApp(
+  {
+    model: "",
+    find,
+    update,
+  },
+  {},
+  session,
+);
 
 describe("Update User", () => {
-  const session = jest.fn((options) => (req, res, next) => {
-    req.session = { user: "1" };
-    next();
-  });
-  const update = jest.fn((_, username) => Object.assign({ _id: "1" }, username));
-  const find = jest.fn(() => ({ username: "jerry", _id: "1" }));
-  const app = MakeApp(
-    {
-      model: "",
-      find,
-      update,
-    },
-    {},
-    session,
-  );
-  test("update is called", async () => {
+  test("Details should be updated", async () => {
     const response = await request(app).patch("/details").field("username", "jimmy");
     expect(update.mock.calls.length).toBe(1);
+    expect(update).toHaveReturnedWith(Object.assign(startingUser, { username: "terry" }));
   });
-  test("Details are updated", async () => {
+  test("Update should not error", async () => {
     const response = await request(app).patch("/details").field("username", "terry");
-    expect(update).toHaveReturnedWith({ username: "terry", _id: "1" });
-    expect(response.statusCode).toBe(200)
-    expect(response.text).toEqual("Update Successful")
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toEqual("Update Successful");
+  });
+  test("Appropriate details should be moved to locals", async () => {
+    const response = await request(app)
+      .patch("/details")
+      .field("username", "jimbo")
+      .field("password", "123412344s");
+    expect(update).toHaveReturnedWith(
+      Object.assign(startingUser, { username: "jimbo", password: "123412344s" }),
+    );
+    expect(app.locals.username).toEqual("jimbo");
+    expect(app.locals.password).not.toBeDefined();
   });
 });
